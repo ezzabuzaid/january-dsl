685b94304a87995a2992f1a0215f5ffb4b28859d
ezzabuzaid <Ezzabuzaid@hotmail.com>
2024-12-08 16:26:24 +0300

    Initial commit

---
+++ apps/dsl-e2e/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  displayName: 'dsl-e2e',
+  preset: '../../jest.preset.js',
+  setupFiles: ['<rootDir>/src/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/dsl-e2e',
+};
